1ab25689dde21443427c8984c60ef605b40c0a31
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-05-25 11:54:41 +0400

    File structure for scss files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,42 @@
-
+import RightArrow from '../icons/right-arrow.svg';
 
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
+        <nav className="navbar navbar-expand-lg">
+            <div className="container">
+                <a className="navbar-brand text-blue-dark text-capitalize" href="/">bandage</a>
+
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
+
                 <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">Features</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">Pricing</a>
-                    </li>
-                </ul>
+
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link text-gray text-capitalize" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-gray text-capitalize" href="/">product</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-gray text-capitalize" href="/">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-gray text-capitalize" href="/">contact</a>
+                        </li>
+                    </ul>
+
+                    <ul className="navbar-nav align-items-center">
+                        <li className="nav-item">
+                            <a className="nav-link text-blue-light text-capitalize" href="/">login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="btn border-0 btn-primary bg-blue-light text-white" href="/">become a member <img src={RightArrow} alt="Right arrow" /> </a>
+                        </li>
+                    </ul>
+
                 </div>
             </div>
         </nav>
