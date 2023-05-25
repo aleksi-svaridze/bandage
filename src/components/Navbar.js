5e8dc61ea1a486c3c48d696a7c5028248ab857fd
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-05-25 14:26:32 +0400

    Finished: Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,18 @@ import RightArrow from '../icons/right-arrow.svg';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg">
+        <nav className="navbar navbar-expand-lg py-20">
             <div className="container">
-                <a className="navbar-brand text-blue-dark text-capitalize" href="/">bandage</a>
+
+                <a className="navbar-brand text-blue-dark text-capitalize m-0" href="/">bandage</a>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
 
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav column-gap-2 me-auto ms-115">
                         <li className="nav-item">
                             <a className="nav-link text-gray text-capitalize" href="/">Home</a>
                         </li>
@@ -28,16 +29,17 @@ const Navbar = () => {
                         </li>
                     </ul>
 
-                    <ul className="navbar-nav align-items-center">
+                    <ul className="navbar-nav column-gap-4 align-items-lg-center">
                         <li className="nav-item">
                             <a className="nav-link text-blue-light text-capitalize" href="/">login</a>
                         </li>
                         <li className="nav-item">
-                            <a className="btn border-0 btn-primary bg-blue-light text-white" href="/">become a member <img src={RightArrow} alt="Right arrow" /> </a>
+                            <a className="text-decoration-none d-flex align-items-center justify-content-center column-gap-3 border-0 btn-primary bg-blue-light text-white" href="/">Become a member <img src={RightArrow} alt="Right arrow" /> </a>
                         </li>
                     </ul>
 
                 </div>
+
             </div>
         </nav>
     )
